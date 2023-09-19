0613a438b6d7352d71bbb3103bbd02a1be2feadd
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-19 15:47:32 +0200

    Ejercicio 02

--- ejercicios-react/src/App.js
+++ ejercicios-react/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Ejercicio01 from './ejercicios/Ejercicio01';
+import Ejercicio02 from './ejercicios/Ejercicio02';
 
 function App() {
   return (
     <div className="App">
       <Ejercicio01/>
+      <Ejercicio02/>
     </div>
   );
 }
---
+++ ejercicios-react/src/ejercicios/Ejercicio02.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Ejercicio02() {
+    const name = "Barbara";
+    const hello = <h1>Hello,{name}</h1>;
+
+  return (
+    <div>
+      {hello}
+    </div>
+  );
+}
+
+export default Ejercicio02;
