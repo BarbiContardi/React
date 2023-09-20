fcc9ad8757236f27e7de1ade6c14737d81320048
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-20 12:33:28 +0200

    Ejercicio 03

--- ejercicios-react/src/App.js
+++ ejercicios-react/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Ejercicio01 from './ejercicios/Ejercicio01';
 import Ejercicio02 from './ejercicios/Ejercicio02';
+import Ejercicio03 from './ejercicios/Ejercicio03';
 
 function App() {
   return (
     <div className="App">
       <Ejercicio01/>
       <Ejercicio02/>
+      <Ejercicio03/>
     </div>
   );
 }
---
+++ ejercicios-react/src/ejercicios/Ejercicio03.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Ejercicio03() {
+  const respuesta = <p> Si la variable name no se pasa a la funcion esta seria undefined</p>;
+
+  return (
+    <div>
+      {respuesta}
+    </div>
+  );
+}
+
+export default Ejercicio03;
