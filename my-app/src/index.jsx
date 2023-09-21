954eb323d4583593a6a4e945f645e66b3bbdc822
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-21 13:49:59 +0200

    Ejercicio 09

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,6 +1,6 @@
 import './index.css'
 import App from "./App";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
 
 const root = createRoot(document.getElementById('root'))
