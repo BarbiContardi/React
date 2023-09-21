c7ff4e18817e97f6ae09443ca154205abf8e90f8
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-21 10:09:08 +0200

    Ejercicio 05

--- ejercicios-react/src/App.js
+++ ejercicios-react/src/App.js
@@ -4,6 +4,7 @@ import Ejercicio01 from './ejercicios/Ejercicio01';
 import Ejercicio02 from './ejercicios/Ejercicio02';
 import Ejercicio03 from './ejercicios/Ejercicio03';
 import Ejercicio04 from './ejercicios/Ejercicio04';
+import Ejercicio05 from './ejercicios/Ejercicio05';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Ejercicio02 name= "Barbara"/>
       <Ejercicio03/>
       <Ejercicio04/>
+      <Ejercicio05/>
     </div>
   );
 }
---
+++ ejercicios-react/src/ejercicios/Ejercicio05.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ejercicio05 = () => {
+    const sum = (a, b) => a + b
+  return (
+    <h2>El resultado es {sum(5,4)}</h2>
+  )
+}
+
+export default Ejercicio05
+
+
