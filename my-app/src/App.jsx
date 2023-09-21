45d0e25bbe717d9e2ab5c1124583eed6418f0c9b
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-21 14:21:45 +0200

    Ejercicio 14

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,11 @@
-import Hello from "./ejercicios/Ejercicio";
+import Hello from "./ejercicios/Hello";
+import Message from "./ejercicios/Message";
 
 function App() {
     return (
       <div className="App">
         <Hello/>
-        <Hello/>
+        <Message/>
       </div>
     );
   }
--- my-app/src/ejercicios/Hello.jsx
+++ my-app/src/ejercicios/Hello.jsx
@@ -1,10 +1,7 @@
-import Message from "./Ejercicio12"
+
 function Hello() {
   return (
-    <div>
     <h2>Hello, World!</h2>
-    <Message/>
-    </div>
   )
 }
 
