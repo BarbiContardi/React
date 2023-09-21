24eac812612fd3366713ac3c1a775ccd6c3ab072
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-21 10:00:58 +0200

    Ejercicio 04

--- ejercicios-react/src/App.js
+++ ejercicios-react/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Ejercicio01 from './ejercicios/Ejercicio01';
 import Ejercicio02 from './ejercicios/Ejercicio02';
 import Ejercicio03 from './ejercicios/Ejercicio03';
+import Ejercicio04 from './ejercicios/Ejercicio04';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Ejercicio01/>
       <Ejercicio02 name= "Barbara"/>
       <Ejercicio03/>
+      <Ejercicio04/>
     </div>
   );
 }
---
+++ ejercicios-react/src/ejercicios/Ejercicio04.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Ejercicio04() {
+  const respuesta = <p>Se puede utilizar llaves para encerrar el nombre, siempre y cuando dentro haya un string, sino dara error</p>;
+
+  return (
+    <div>
+      {respuesta}
+    </div>
+  );
+}
+
+export default Ejercicio04;
