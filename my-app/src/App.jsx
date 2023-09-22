38e3ed21887b1811f9a7bc11fb4f911d651fe748
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-22 11:58:21 +0200

    Ejercicio 16

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Hello from "./ejercicios/Hello";
 import Message from "./ejercicios/Message";
+import Welcome from "./ejercicios/Welcome";
 
 function App() {
     return (
       <div className="App">
         <Hello/>
         <Message/>
+        <Welcome name = "Barbi" />
       </div>
     );
   }
---
+++ my-app/src/ejercicios/Welcome.jsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line react/prop-types
+export default function Welcome({ name }) {
+  return <h2>Welcome, {name}!</h2>;
+}
