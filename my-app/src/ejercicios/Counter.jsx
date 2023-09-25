f20dad74f3999cd8a8a1206f9665168816599190
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-25 13:25:09 +0200

    Ejercicio 31

--- my-app/src/ejercicios/Counter.jsx
+++ my-app/src/ejercicios/Counter.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import Button from "./Button";
 
-export default function Counter(){
-    const [counter, setCounter] = useState(0);
-    function handleCounterIncrement(){
-        setCounter((c)=> c + 1);
-//se puede usar una arrow function sin problema pero        
-//es una práctica es más común y recomendable pasar un valor inmediato como parámetro a la función "setter" para 
-//incrementar el contador, ya que hace que el código sea más claro y menos propenso a errores.
-    }
-    return(
-        <div>
-            <h2>I have counted to {counter}</h2>
-            <Button onClick={handleCounterIncrement} label={'Increment'}/>
-        </div>
-    )
+// eslint-disable-next-line react/prop-types
+export default function Counter({ initialValue = 0 }) {
+  const [counter, setCounter] = useState(initialValue);
+  function handleCounterIncrement() {
+    setCounter((c) => c + 1);
+  }
+  return (
+    <div>
+      <h2>I have counted to {counter}</h2>
+      <Button onClick={handleCounterIncrement} label={"Increment"} />
+    </div>
+  );
 }
