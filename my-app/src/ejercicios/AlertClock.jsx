3244c893df6aef344a6e968be950440d3a5aa8b7
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-25 11:14:56 +0200

    Ejercicio 28

--- my-app/src/ejercicios/AlertClock.jsx
+++ my-app/src/ejercicios/AlertClock.jsx
@@ -1,12 +1,14 @@
-export function AlertClock() {
-    function handleShowTime(){
-     alert('Current time:' + new Date())
-    }
-     return(
-        <div>
-            <p>Click to see the current time:</p>
-            <button onClick={handleShowTime}>Click Me!</button>
-        </div>
-     )
+import Button  from "./Button";
+
+
+export default function AlertClock() {
+  function handleShowTime() {
+    alert("Current time:" + new Date());
+  }
+  return (
+    <div>
+      <p>Click to see the current time:</p>
+      <Button onClick={handleShowTime} label={"Click Me!"} />
+    </div>
+  );
 }
-    
---
+++ my-app/src/ejercicios/Button.jsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line react/prop-types
+export default function Button({label , onClick}){
+ return <button onClick={onClick}>{label}</button>
+}
