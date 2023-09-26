5f9cdb533427f46e91f9755867a86009d8d837f7
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-26 13:34:49 +0200

    Ejercicio 38

--- my-app/src/ejercicios/MouseClicker.jsx
+++ my-app/src/ejercicios/MouseClicker.jsx
@@ -2,10 +2,15 @@ export default function MouseClicker() {
   function handleButtonClick(event) {
     console.log(event.currentTarget.name);
   }
+  function handleImgClicker(event){
+    console.log(event.target.src)
+  }
   return (
     <button name="one" onClick={handleButtonClick}>
       Click Me!
-      <img height={24} width={24} />
+      <img src="./public/vite.svg" height={24} width={24} onClick={handleImgClicker} />
     </button>
   );
 }
+//se sigue imprimiendo el name al hacer click en la imagen porque ésta esta dentro del boton
+//al hacer click se hace "bubble" hasta llegar al event del boton y se imprime
