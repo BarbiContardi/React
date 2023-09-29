3b7351e88dfd45fdf4db365a68a2c441cdc6eb6c
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-29 13:47:13 +0200

    Ejercicio 8/Uncontrolled Forms

--- my-app/src/ejercicios/UncontrolledLogin.jsx
+++ my-app/src/ejercicios/UncontrolledLogin.jsx
@@ -1,7 +1,7 @@
 export default function UncontrolledLogin() {
     function handleFormSubmit(event) {
         event.preventDefault();
-        const username = event.target.elements.username.value;
+        const username = event.target.elements.username.value; //utilizo Api DOM para acceder a los datos del formulario
         const password = event.target.elements.password.value;
         const data = { username, password };
         console.log(data);
@@ -18,3 +18,6 @@ export default function UncontrolledLogin() {
     </form>
   );
 }
+//la API estándar de React DOM es fundamental para interactuar con el DOM en aplicaciones React,
+// pero también existen API no estándar, como las bibliotecas de manejo de formularios Formik o react-hook-form,
+// que pueden ser útiles para acceder y gestionar datos de formularios de manera más eficiente.
