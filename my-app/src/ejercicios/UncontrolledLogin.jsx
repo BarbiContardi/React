0a5db5900b9c371bf9ddb004a62b5313cd638af2
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-29 13:30:47 +0200

    Ejercicio 7/Uncontrolled Forms

--- my-app/src/ejercicios/UncontrolledLogin.jsx
+++ my-app/src/ejercicios/UncontrolledLogin.jsx
@@ -1,12 +1,20 @@
-export default function UncontrolledLogin(){
-      return(
-        <form>
-            <h1>My Uncontrolled Form</h1>
-            <input type="text" name="username" />
-            <input type="password" name="password" />
-            <input type="checkbox" name="session"/>
-            <button>Login</button>
-            <button type="reset">Reset</button>
-        </form>
-    )
+export default function UncontrolledLogin() {
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        const username = event.target.elements.username.value;
+        const password = event.target.elements.password.value;
+        const data = { username, password };
+        console.log(data);
+      }
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <h1>My Uncontrolled Form</h1>
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <input type="checkbox" name="session" />
+      <button>Login</button>
+      <button type="reset">Reset</button>
+    </form>
+  );
 }
