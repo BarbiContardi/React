b1c3e56b735980fb403c1415204e8f816b4c3ef0
Barbara Contardi <barbi.contardi@gmail.com>
2023-10-09 11:20:30 +0200

    Ejercicio 52/ Refs

--- my-app/src/ejercicios/Counter.jsx
+++ my-app/src/ejercicios/Counter.jsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Button from "./Button";
 import CounterDisplay from "./CounterDisplay";
+
 
 // eslint-disable-next-line react/prop-types
 export default function Counter({ initialValue = 0 }) {
   const [counter, setCounter] = useState(initialValue);
+  const directionRef = useRef(null);
 
   useEffect(() => {
-    console.log("El valor actual es: ", counter);
-  }, [counter]);
+    if (!directionRef.current) {
+      const direction = counter > initialValue ? 'up' : 'down';
+      console.log(`La dirección del cambio es: ${direction}`);
+    }
+  }, [counter, initialValue]);
 
-  function handleCounterIncrement() {
-    setCounter((c) => c + 1);
-  }
-  function handleCounterDecrement() {
-    setCounter((c) => c - 1);
-  }
-  function handleCounterReset() {
-    setCounter(initialValue);
-  }
+  const handleCounterIncrement = () => setCounter(counter + 1);
+  const handleCounterDecrement = () => setCounter(counter - 1);
+  const handleCounterReset = () => setCounter(initialValue);
   return (
     <div>
       <CounterDisplay counter={counter} />
