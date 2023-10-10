2868b2eeeb27cd8a63e4b8281bded36b12084d02
Barbara Contardi <barbi.contardi@gmail.com>
2023-10-10 11:40:16 +0200

    Ejercicio 60/Styling Components

--- my-app/src/ejercicios/Welcome.jsx
+++ my-app/src/ejercicios/Welcome.jsx
@@ -3,7 +3,7 @@ import Age from "./Age";
 // eslint-disable-next-line react/prop-types
 export default function Welcome({ name}) {
   return (
-    <div>
+    <div className="welcome">
       <p>Welcome, {<strong>{name}</strong>}!</p>
       <Age age= {0}/>
     </div>
