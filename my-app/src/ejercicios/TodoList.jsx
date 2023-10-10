57709b418fdf76ddc0920653695f184dadcd5b9c
Barbara Contardi <barbi.contardi@gmail.com>
2023-10-10 11:25:19 +0200

    Ejercicio 59/Lists and State

--- my-app/src/ejercicios/TodoList.jsx
+++ my-app/src/ejercicios/TodoList.jsx
@@ -1,36 +1,44 @@
 import { useState } from "react";
 
-export default function TodoList(){
-        const [items, setItems] = useState([]);
-        const [inputValue, setInputValue] = useState('');
-      
-        const handleAddItem = () => {
-          if (inputValue.trim() !== '') {
-            setItems([...items, inputValue]);
-            setInputValue('');
-          }
-        };
-        const handleReset = () => {
-            setItems([]); 
-        };
-      
-        return (
-          <div>
-            <h2>Lista de Tareas</h2>
-            <ul>
-              {items.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-            <div>
-              <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-              <button onClick={handleAddItem}>Agregar</button>
-              <button onClick={handleReset}>Reset</button>
-            </div>
-          </div>
-    )
+export default function TodoList() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== "") {
+      setItems([...items, inputValue]);
+      setInputValue("");
+    }
+  };
+  const handleReset = () => {
+    setItems([]);
+  };
+  const handleRemoveItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div>
+      <h2>Lista de Tareas</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleRemoveItem(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={handleAddItem}>Agregar</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
 }
