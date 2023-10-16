6f1dd4ba0379f58d2fe85fa2b39c8b6508adca28
Barbara Contardi <barbi.contardi@gmail.com>
2023-10-16 14:40:26 +0200

    Ejercicio 78/Custom Hooks

--- my-app/src/ejercicios/useGithubUser.jsx
+++ my-app/src/ejercicios/useGithubUser.jsx
@@ -3,16 +3,21 @@ import { useEffect, useState } from "react";
 
 export default function useGithubUser(username) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError]= useState(null)
 
   function fetchGitHubUser(username) {
+    setLoading(true)
+    setError(null)
     axios
       .get(`https://api.github.com/users/${username}`)
       .then((response) => setData(response.data))
-      .catch((e) => console.error("Error", e));
+      .catch((e) => setError(e), setData(null))
+      .finally(setLoading(false));
   }
   useEffect(() => {
     fetchGitHubUser(username);
   }, [username]);
 
-  return data;
+  return {data , loading, error};
 }
