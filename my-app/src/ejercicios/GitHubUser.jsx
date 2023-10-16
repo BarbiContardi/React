ef6acdc8feaafa0acf6ad5f114aa5042d8d0378e
Barbara Contardi <barbi.contardi@gmail.com>
2023-10-16 14:33:22 +0200

    Ejercicio 77/Custom Hooks

--- my-app/src/ejercicios/GitHubUser.jsx
+++ my-app/src/ejercicios/GitHubUser.jsx
@@ -1,21 +1,9 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+import useGithubUser from "./useGithubUser";
 
 export default function GitHubUser({ username }) {
-  const [data, setData] = useState(null);
-  async function fetchGitHubUser(username) {
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-      console.log(json);
-      setData(json);
-    } catch (error) {
-      console.log("Error", error);
-    }
-  }
-  useEffect(() => {
-    fetchGitHubUser(username);
-  }, [username]);
+  const data = useGithubUser(username);
+
   return (
     <div>
       {data && (
