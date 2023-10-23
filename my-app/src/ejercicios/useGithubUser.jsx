88c9ffc8343a46d56c082a36d1becdecd010b46d
Barbara Contardi <barbi.contardi@gmail.com>
2023-10-23 12:58:41 +0200

    Ejercicio 91/SWR

--- my-app/src/ejercicios/useGithubUser.jsx
+++ my-app/src/ejercicios/useGithubUser.jsx
@@ -2,21 +2,22 @@ import axios from "axios";
 import useSWR from "swr";
 
 function fetchGitHubUser(username) {
-  if (username) {
-    return axios
-      .get(`https://api.github.com/users/${username}`)
-      .then((response) => response.data);
-  } else {
-    return null; 
-  }
+  return axios
+    .get(`https://api.github.com/users/${username}`)
+    .then((response) => response.data);
 }
 
 export default function useGithubUser(username) {
-  const { data, error } = useSWR(username, fetchGitHubUser);
+  const { data, error, mutate } = useSWR(username, fetchGitHubUser);
+
+  const refetch = () => {
+    mutate();
+  };
 
   return {
     data,
     loading: !data && !error,
     error,
+    refetch,
   };
 }
